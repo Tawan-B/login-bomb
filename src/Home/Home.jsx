54108fa380201e0c5daf919e652e2ffe051f9e07
Tawan Barbosa da Silva <tawan.silva@maisunifacisa.com.br>
2025-09-26 21:37:32 -0300

    primeiro commit

---
+++ src/Home/Home.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import './Home.css';
+
+function Home() {
+    const [products, setProducts] = useState([
+        { id: 1547, name: 'Fire Up', price: 1, image: '/fireup.PNG' },
+        { id: 2898, name: 'Power Glove', price: 99999, image: '/powerglove.PNG' },
+        { id: 2454, name: 'Remote Control', price: 500, image: '/remotecontrol.PNG'},
+    ]);
+
+    const removeProduct = (productId) => {
+        const updatedProducts = products.filter(product => product.id !== productId);
+        setProducts(updatedProducts);
+    };
+
+    return (
+        <div className="home-container">
+            <h1>Main Stage – Power-ups</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th style={{ width: '50px' }}></th> 
+                        <th>Code</th>
+                        <th>Name</th>
+                        <th>Price</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {products.map(product => (
+                        <tr key={product.id}>
+                            <td>
+                                <img 
+                                    src={product.image} 
+                                    alt={product.name} 
+                                    className="product-icon"
+                                />
+                            </td>
+                            <td>{product.id}</td>
+                            <td>{product.name}</td>
+                            <td>R$ {product.price.toFixed(2)}</td>
+                            <td>
+                                <button onClick={() => removeProduct(product.id)}>
+                                    Remove
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/login/Login.jsx
@@ -0,0 +1,111 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Login.css";
+
+const Login = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginMessage, setLoginMessage] = useState("");
+
+  const users = [
+    {
+      name: "Bruno",
+      password: "coxinha123",
+      email: "bruno@maisunifacisa.com.br",
+      cpf: "111.111.111-11"
+    },
+    {
+      name: "Tawan",
+      password: "teste789",
+      email: "tawan@maisunifacisa.com.br",
+      cpf: "222.222.222-22"
+    },
+    {
+      name: "José",
+      password: "ponto456",
+      email: "jose@maisunifacisa.com.br",
+      cpf: "333.333.333-33"
+    }
+  ];
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    const userFound = users.find(
+      (user) => user.email === email && user.password === password
+    );
+
+    if (userFound) {
+      setLoginMessage("Usuário logado com sucesso!");
+      setTimeout(() => navigate("/home"), 1000); 
+    } else {
+      setLoginMessage("Usuário não encontrado");
+    }
+  };
+
+  return (
+    <div id="login-page" className="login-page">
+      <div className="login-container">
+        <div className="left-container">
+          <h1>Sign In To Blast Zone</h1>
+          
+          <div className="form-container">
+            <h5>Or use your e-mail account</h5>
+            <form onSubmit={handleLogin}>
+              
+              <div className="form-group">
+                <i className="fas fa-envelope"></i> 
+                <input
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  type="text"
+                  id="userEmail"
+                  name="userEmail"
+                  placeholder="userEmail"
+                  required
+                />
+              </div>
+
+              <div className="form-group">
+                <i className="fas fa-lock"></i> 
+                <input
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  type="password"
+                  id="password"
+                  name="password"
+                  placeholder="password"
+                  required
+                />
+              </div>
+              
+              <button className="submit-button" type="submit">Login</button>
+            </form>
+            
+            {loginMessage && 
+                <p className={`login-message ${loginMessage.includes('sucesso') ? 'success' : 'error'}`}>
+                    {loginMessage}
+                </p>
+            }
+          </div>
+        </div>
+        
+        <div className="right-container">
+            <img src='/bombermanLike.gif' alt="Personagem de Pixel Art" className="gif-personagem"/>
+            <h1>Go Bomb!</h1>
+            <p>Ready to join the Bomber crew? Fill your details and start the adventure!</p>
+            <button
+                onClick={() => {
+                window.location.href = "/register";
+                }}
+            >
+                Sign Up
+            </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
